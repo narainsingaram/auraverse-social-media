936a4614b5a18e8e57cf2755fe5ceaf448f70de6
Nithin Venkatesh <nvenk2006@gmail.com>
2023-07-24 16:28:57 -0400

    yunrain

--- client/pages/user/dashboard.jsx
+++ client/pages/user/dashboard.jsx
@@ -1,5 +1,6 @@
 import { useContext } from 'react';
 import { UserContext } from '../../context';
+
 import {
 Card,
 Spacer,
@@ -12,17 +13,31 @@ Checkbox,
 Container,
 useInput,
 } from "@nextui-org/react";
+;
+///import UserRoute from "../../components/routes/UserRoute";
+///import CreatePostForm from "../../components/forms/CreatePortForm";
+
 
 const Dashboard = () => {
+    const[state,setState ] = useContext(UserContext);
     return (
-        <Container>
-            <Container>
-                <h1 className="my-16 bg-blue-300 p-4 rounded-xl text-2xl">Dashboard Page</h1>
-            </Container>
-            <Container>
-                <p className="my-16 bg-blue-300 p-4 rounded-xl">This is the Dashboard Page of Auraverse and can only be viewed by registered users.</p>
-            </Container>
-        </Container>
+        // <Container>
+        //     <Container>
+        //         <h1 className="my-16 bg-blue-300 p-4 rounded-xl text-2xl">Posts</h1>
+        //     </Container>
+        //     <Container>
+        //         <p className="my-16 bg-blue-300 p-4 rounded-xl">This is the Dashboard Page of Auraverse and can only be viewed by registered users.</p>
+        //     </Container>
+        //     <Container className="row py-3">
+        //         <Container className="col-md-8">
+        //             <CreatePostForm />
+        //             </Container>
+        //         <Container className="col-md-4">Sidebar</Container>
+
+
+        //     </Container>
+        // </Container>
+        <div>hi</div>
     );
 };
 

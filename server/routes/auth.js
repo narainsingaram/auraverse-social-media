9e650d92ef5f3733b70ed9c8d5c1d3074e5b1a77
Narain Singaram <narainsingaram7@gmail.com>
2023-07-26 21:16:11 -0400

    TypeError: expressJwt is not a function

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,12 +1,13 @@
-const express = require('express');
-
 const router = express.Router();
+
+//middleware
+import { requireSignIn } from "../middlewares/index.js";
 
 //controllers
 import { register, login, currentUser } from "../controllers/auth";
 
 router.post("/register", register);
 router.post("/login", login);
-router.get("/current-user", currentUser);
+router.get("/current-user", requireSignIn, currentUser);
 
 module.exports = router;

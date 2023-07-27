62ba541a83d58c3a25bf40a50bd8b07c936ee423
Narain Singaram <narainsingaram7@gmail.com>
2023-07-26 21:27:39 -0400

    think i fixed the error about to restart again

--- server/middlewares/index.js
+++
@@ -1,8 +0,0 @@
-const expressJwt = require('express-jwt');
-
-const requireSignIn = expressJwt({ 
-    secret: process.env.JWT_SECRET,
-    algorithms: ["HS256"],
-});
-
-module.exports = requireSignIn;
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,13 +1,11 @@
+const express = require("express");
 const router = express.Router();
-
-//middleware
-import { requireSignIn } from "../middlewares/index.js";
 
 //controllers
 import { register, login, currentUser } from "../controllers/auth";
 
 router.post("/register", register);
 router.post("/login", login);
-router.get("/current-user", requireSignIn, currentUser);
+router.get("/current-user", currentUser);
 
 module.exports = router;
